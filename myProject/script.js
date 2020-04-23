206acd989e14579e734993f14270098dfab76837
RameshSuthar <rameshsuthar1231@gmail.com>
2020-04-23 21:31:03 +0530

    Fix the post content

--- myProject/script.js
+++ myProject/script.js
@@ -2,7 +2,7 @@
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	$(".g-signin2").css("display", "none");
-	$("#sign-out").css("display", "block");
+    $("#sign-out").css("display", "block");
     $(".timeline").css("display", "block");
     $("#name").css("display", "block");
 	$("#pic").attr('src', profile.getImageUrl());
@@ -20,4 +20,34 @@ function signOut()
         $("#name").css("display", "none");
         $("#sign-out").css("display", "none");
     } );
+ }
+
+
+ function post() {
+    var list = document.getElementById('content-list').innerHTML;
+    var name = document.getElementById('name').value;
+    var content = document.getElementById('post-content').value;
+
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+
+    list += `
+        <li>
+            <div class="content">
+                <h3>
+                    ${name}
+                </h3>
+                <p>
+                    ${content}
+                </p>
+            </div>
+            <div class="time">
+                <h4>
+                    ${date}
+                </h4>
+            </div>
+        </li>
+        <div style="clear: both;"></div>
+    `;
+    document.getElementById('content-list').innerHTML = list;
  }
